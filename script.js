3b77c4bdb39dbfb70fde444874fffe206156de21
dasiklasik <daryasamsonowicz@gmail.com>
2023-03-03 15:32:46 +0300

    feat: create check number function

--- script.js
+++ script.js
@@ -2,4 +2,33 @@ const calculateRandomNumber = () => {
     return Math.trunc(1 + Math.random() * 20)
 }
 
+const checkNumber = () => {
+    if(!checkInput.value) {
+        message.textContent = '⛔️ No number'
+    } else if (checkInput.value === randomNumber) {
+        message.textContent = '🎉️ Correct number'
+    } else if (checkInput.value !== randomNumber) {
+        if (score >= 1) {
+            message.textContent = checkInput.value > randomNumber ? 'Too high!' : 'Too low!'
+            score--
+            scoreField.textContent = score.toString()
+        } else {
+            message.textContent = '😭 You lose'
+            score = 0
+            scoreField.textContent = score.toString()
+        }
+    }
+}
+
+const checkInput = document.querySelector('.guess')
+const checkBtn = document.querySelector('.btn.check')
+const message = document.querySelector('.message')
+const number = document.querySelector('.number')
+const scoreField = document.querySelector('.score')
+
 let randomNumber = calculateRandomNumber()
+let score = 20;
+
+checkBtn.addEventListener('click', checkNumber)
+
+number.textContent = randomNumber.toString()
